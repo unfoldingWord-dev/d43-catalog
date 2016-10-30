6dbfcdeb954c9d5ec09d31e3df10a900d334be15
Phil Hopper <phillip_hopper@wycliffeassociates.org>
2016-10-30 16:36:12 -0400

    Issue 247: write signing lambda function
    Sign and verify functions, with tests
    Sign text and zip files on S3
    Update in DynamoDB
    Improve coverage

---
+++ functions/signing/signing.py
@@ -0,0 +1,244 @@
+from __future__ import unicode_literals
+import boto3
+import codecs
+import inspect
+import json
+import os
+import shlex
+import shutil
+import tempfile
+from base64 import b64decode
+from general_tools.file_utils import write_file
+from general_tools.url_utils import download_file
+from subprocess import Popen, PIPE
+
+
+class Signing(object):
+
+    dynamodb_table_name = 'd43-catalog-in-progress'
+
+    @staticmethod
+    def is_travis():
+        return 'TRAVIS' in os.environ and os.environ['TRAVIS'] == 'true'
+
+    @staticmethod
+    def decrypt_file(source_file_name, destination_file_name):
+        """
+        Decrypts a file using the AWS encryption key 'signing_key'
+        :param string source_file_name:
+        :param string destination_file_name:
+        :return: bool True if successful, otherwise False
+        """
+        client = boto3.client('kms')
+
+        with codecs.open(source_file_name, 'rb') as in_file:
+            data_bytes = in_file.read()
+
+        response = client.decrypt(CiphertextBlob=data_bytes)
+
+        if 'Plaintext' not in response:
+            raise Exception('File not successfully decrypted: {}'.format(source_file_name))
+
+        write_file(destination_file_name, response['Plaintext'])
+
+        return True
+
+    @staticmethod
+    def sign_file(file_to_sign, pem_file=None):
+        """
+        Generates a .sig file and returns the full file name of the .sig file
+        :param str|unicode file_to_sign:
+        :param str|unicode|None pem_file:
+        :return: str|unicode The full file name of the .sig file
+        """
+        # if pem file was not passed, use the default one
+        if not pem_file:
+            pem_file = Signing.get_default_pem_file()
+
+        # # read the file contents
+        # with codecs.open(file_to_sign, 'r', encoding='utf-8') as in_file:
+        #     content = in_file.read()
+
+        # use openssl to sign the content
+        sha384_file = file_to_sign + '.sha384'
+        sign_com = 'openssl dgst -sha384 -sign {0} -out {1} {2}'.format(pem_file, sha384_file, file_to_sign)
+        command = shlex.split(sign_com)
+        com = Popen(command, shell=False, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        out, err = com.communicate()
+
+        if err:
+            raise Exception(err)
+
+        # base64 encode the signature
+        file_name_without_extension = os.path.splitext(file_to_sign)[0]
+        sig_file_name = '{}.sig'.format(file_name_without_extension)
+        sign_com = 'openssl base64 -in {0} -out {1}'.format(sha384_file, sig_file_name)
+        command = shlex.split(sign_com)
+        com = Popen(command, shell=False, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        out, err = com.communicate()
+
+        if err:
+            raise Exception(err)
+
+        # get the base64 encoded signature
+        with codecs.open(sig_file_name, 'r', encoding='utf-8') as in_file:
+            signed_content = in_file.read()
+
+        # save the signed content
+        file_content = []
+        signature = {'si': 'uW', 'sig': signed_content}
+        file_content.append(signature)
+        write_file(sig_file_name, file_content)
+
+        return sig_file_name
+
+    @staticmethod
+    def verify_signature(content_file, sig_file, pem_file=None):
+        """
+        Verify that the file content has not changed since it was signed
+        :param str|unicode content_file:
+        :param str|unicode sig_file:
+        :param str|unicode|None pem_file:
+        :return:
+        """
+
+        temp_dir = tempfile.mkdtemp(prefix='tempVerify_')
+
+        try:
+
+            # if pem file was not passed, use the default one
+            if not pem_file:
+                pem_file = os.path.join(temp_dir, 'uW-vk.pem')
+                download_file('https://pki.unfoldingword.org/uW-vk.pem', pem_file)
+
+            # get the uW signature from the sig file
+            with codecs.open(sig_file, 'r', 'utf-8-sig') as in_file:
+                sig_file_content = json.loads(in_file.read())
+
+            signature = [x['sig'] for x in sig_file_content if x['si'] == 'uW'][0]
+            signature_path = os.path.join(temp_dir, 'signature.sig')
+
+            # save the signature to a temp file
+            with open(signature_path, str('w')) as out_file:
+                out_file.write(b64decode(signature))
+
+            # Use openssl to verify signature
+            command_str = 'openssl dgst -sha384 -verify {0} -signature {1} {2}'.format(pem_file, signature_path,
+                                                                                       content_file)
+            command = shlex.split(command_str)
+            com = Popen(command, shell=False, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+            out, err = com.communicate()
+
+            return out.strip() == 'Verified OK'
+
+        finally:
+            # clean up temp dir, if used
+            if temp_dir and os.path.isdir(temp_dir):
+                shutil.rmtree(temp_dir, ignore_errors=True)
+
+    @staticmethod
+    def get_default_pem_file():
+        """
+        Decrypts the default pem file and returns the full file name
+        :return: str|unicode
+        """
+        this_dir = os.path.dirname(inspect.stack()[0][1])
+        enc_file = os.path.join(this_dir, 'uW-sk.enc')
+        pem_file = os.path.join(this_dir, 'uW-sk.pem')
+        result = Signing.decrypt_file(enc_file, pem_file)
+
+        if not result:
+            raise Exception('Not able to decrypt the pem file.')
+
+        return pem_file
+
+    @staticmethod
+    def handle_s3_trigger(event, s3_handler, dynamodb_handler, logger, private_pem_file=None, public_pem_file=None):
+        """
+        Handles the signing of a file on S3
+        :param dict event:
+        :param class s3_handler: This is passed in so it can be mocked for unit testing
+        :param class dynamodb_handler: This is passed in so it can be mocked for unit testing
+        :param logger:
+        :param string private_pem_file:
+        :param string public_pem_file:
+        :return: bool
+        """
+
+        # this shouldn't happen, but just in case
+        if 'Records' not in event:
+            if logger:
+                logger.warning('The signing script was triggered but no `Records` were found.')
+            return False
+
+        temp_dir = tempfile.mkdtemp(prefix='signing_')
+
+        try:
+            for record in event['Records']:
+
+                # check if this is S3 bucket record
+                if 's3' not in record:
+                    if logger:
+                        logger.warning('The record is not an S3 bucket.')
+                    return False
+
+                bucket_name = record['s3']['bucket']['name']
+                key = record['s3']['object']['key']
+
+                # detect test bucket
+                is_test = bucket_name.startswith('test-')
+
+                cdn_bucket_name = ('test-' if is_test else '') + 'cdn.door43.org'
+                cdn_handler = s3_handler(cdn_bucket_name)
+
+                # get the file name
+                base_name = os.path.basename(key)
+                file_name, file_extension = os.path.splitext(base_name)
+
+                # copy the file to a temp directory
+                file_to_sign = os.path.join(temp_dir, base_name)
+                cdn_handler.download_file(key, file_to_sign)
+
+                # sign the file
+                sig_file = Signing.sign_file(file_to_sign, pem_file=private_pem_file)
+
+                # verify the file
+                verified = Signing.verify_signature(file_to_sign, sig_file, pem_file=public_pem_file)
+
+                if not verified:
+                    if logger:
+                        logger.warning('The signature was not successfully verified.')
+                    return False
+
+                # upload the sig file to the S3 bucket
+                upload_name = key.replace(file_extension, '.sig')
+                cdn_handler.upload_file(sig_file, upload_name)
+
+                # update the record in DynamoDB
+                key_parts = key.split('/')  # test/repo-name/commit/file.name
+                repo_name = key_parts[1]
+                commit_id = key_parts[2]
+                db_handler = dynamodb_handler(Signing.dynamodb_table_name)
+                record_keys = {'repo_name': repo_name, 'commit_id': commit_id}
+                row = db_handler.get_item(record_keys)
+
+                # add the sig file to the files list
+                row['files'].append(upload_name)
+
+                # add the url of the sig file to the format item
+                data = json.loads(row['data'])
+                for fmt in data['formats']:
+                    if fmt['url'].endswith(key_parts[3]):
+                        extension_length = len(os.path.splitext(key_parts[3])[1])
+                        fmt['sig'] = '{}.sig'.format(fmt['url'][:(extension_length * -1)])
+                        break
+
+                row['data'] = json.dumps(data, sort_keys=True)
+                db_handler.update_item(record_keys, row)
+
+            return True
+
+        finally:
+            if os.path.isdir(temp_dir):
+                shutil.rmtree(temp_dir, ignore_errors=True)
+
---
+++ tests/signing/test_signing.py
@@ -0,0 +1,329 @@
+from __future__ import unicode_literals, print_function
+import codecs
+import json
+import os
+import shutil
+import tempfile
+import unittest
+import uuid
+from unittest import TestCase
+from datetime import datetime
+from aws_tools.dynamodb_handler import DynamoDBHandler
+from aws_tools.s3_handler import S3Handler
+from general_tools.file_utils import load_json_object
+
+from functions.signing.signing import Signing
+
+
+class TestSigning(TestCase):
+
+    resources_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'resources')
+
+    class MockLogger(object):
+
+        @staticmethod
+        def warning(message):
+            print('WARNING: {}'.format(message))
+
+    class MockS3Handler(object):
+
+        def __init__(self, bucket_name):
+            self.bucket_name = bucket_name
+
+        @staticmethod
+        def download_file(key, local_file):
+            shutil.copy(key, local_file)
+
+        @staticmethod
+        def upload_file(path, key):
+            shutil.copy(path, key)
+
+    class MockDynamodbHandler(object):
+
+        def __init__(self, table_name):
+            self.table_name = table_name
+
+        # noinspection PyUnusedLocal
+        @staticmethod
+        def get_item(record_keys):
+            return load_json_object(os.path.join(TestSigning.resources_dir, 'dynamodb_record.json'))
+
+        # noinspection PyUnusedLocal
+        @staticmethod
+        def update_item(record_keys, row):
+            return True
+
+    def setUp(self):
+        self.temp_dir = tempfile.mkdtemp(prefix='unitTest_')
+
+    def tearDown(self):
+        if os.path.isdir(self.temp_dir):
+            shutil.rmtree(self.temp_dir, ignore_errors=True)
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    @staticmethod
+    def create_event():
+
+        event = {'Records': []}
+
+        return event
+
+    @staticmethod
+    def create_s3_record(bucket_name, object_key):
+
+        record = {
+            's3': {
+                'bucket': {'name': bucket_name},
+                'object': {'key': object_key}
+            }
+        }
+
+        return record
+
+    @unittest.skipIf(Signing.is_travis(), 'Skipping test_decrypt_file on Travis CI.')
+    def test_decrypt_file(self):
+        """
+        This tests the decryption of an encrypted file. The lambda function will use this to decrypt the key file
+        that is used to sign source files
+        :return:
+        """
+
+        encrypted_file = os.path.join(self.resources_dir, 'test.enc')
+        decrypted_file = os.path.join(self.temp_dir, 'test.txt')
+        result = Signing.decrypt_file(encrypted_file, decrypted_file)
+
+        # verify the file was decrypted
+        self.assertTrue(result)
+        self.assertTrue(os.path.isfile(decrypted_file))
+
+        # verify the contents
+        with codecs.open(decrypted_file, 'r', encoding='utf-8') as in_file:
+            decrypted_text = in_file.read()
+
+        with codecs.open(os.path.join(self.resources_dir, 'test.txt'), 'r', encoding='utf-8') as in_file:
+            original_text = in_file.read()
+
+        self.assertEqual(decrypted_text, original_text)
+
+    def test_sign_file_with_test_certificate(self):
+
+        # initialization
+        source_file = os.path.join(self.temp_dir, 'source.json')
+        sig_file = os.path.join(self.temp_dir, 'source.sig')
+
+        # copy test file to the temp directory
+        shutil.copy(os.path.join(self.resources_dir, 'source.json'), source_file)
+
+        # check that the source file exists in the temp directory
+        self.assertTrue(os.path.isfile(source_file))
+
+        # check that .sig file DOES NOT exist
+        self.assertFalse(os.path.isfile(sig_file))
+
+        # sign the file
+        sig_file_name = Signing.sign_file(source_file,
+                                          pem_file=os.path.join(self.resources_dir, 'unit-test-private.pem'))
+
+        # check that .sig file DOES exist now
+        self.assertEqual(sig_file, sig_file_name)
+        self.assertTrue(os.path.isfile(sig_file))
+
+        # verify the .sig file is correct
+        self.assertTrue(Signing.verify_signature(source_file, sig_file_name,
+                                                 pem_file=os.path.join(self.resources_dir, 'unit-test-public.pem')))
+
+    @unittest.skipIf(Signing.is_travis(), 'Skipping test_sign_file_with_live_certificate on Travis CI.')
+    def test_sign_file_with_live_certificate(self):
+
+        # initialization
+        source_file = os.path.join(self.temp_dir, 'source.json')
+        sig_file = os.path.join(self.temp_dir, 'source.sig')
+
+        # copy test file to the temp directory
+        shutil.copy(os.path.join(self.resources_dir, 'source.json'), source_file)
+
+        # check that the source file exists in the temp directory
+        self.assertTrue(os.path.isfile(source_file))
+
+        # check that .sig file DOES NOT exist
+        self.assertFalse(os.path.isfile(sig_file))
+
+        # sign the file
+        sig_file_name = Signing.sign_file(source_file)
+
+        # check that .sig file DOES exist now
+        self.assertEqual(sig_file, sig_file_name)
+        self.assertTrue(os.path.isfile(sig_file))
+
+        # verify the .sig file is correct
+        self.assertTrue(Signing.verify_signature(source_file, sig_file_name))
+
+    def test_get_default_pem_file(self):
+
+        if Signing.is_travis():
+            with self.assertRaises(Exception) as context:
+                Signing.get_default_pem_file()
+
+                self.assertEqual('Not able to decrypt the pem file.', str(context.exception))
+
+        else:
+            pem_file = Signing.get_default_pem_file()
+
+            self.assertTrue(pem_file.endswith('uW-sk.pem'))
+            self.assertTrue(os.path.isfile(pem_file))
+
+    def test_openssl_exception_while_signing(self):
+
+        # initialization
+        source_file = os.path.join(self.temp_dir, 'source.json')
+        sig_file = os.path.join(self.temp_dir, 'source.sig')
+
+        # copy test file to the temp directory
+        shutil.copy(os.path.join(self.resources_dir, 'source.json'), source_file)
+
+        # check that the source file exists in the temp directory
+        self.assertTrue(os.path.isfile(source_file))
+
+        # check that .sig file DOES NOT exist
+        self.assertFalse(os.path.isfile(sig_file))
+
+        # sign the file using bogus key
+        with self.assertRaises(Exception) as context:
+            Signing.sign_file(source_file, pem_file=os.path.join(self.resources_dir, 'none.pem'))
+
+        self.assertIn('key file', str(context.exception))
+
+    def test_signing_handler_text(self):
+
+        # copy zip file to temp directory
+        test_txt = os.path.join(self.temp_dir, 'test.txt')
+        shutil.copy(os.path.join(self.resources_dir, 'test.txt'), test_txt)
+
+        # mock a lambda event object
+        event = self.create_event()
+        event['Records'].append(self.create_s3_record('test-cdn_bucket', test_txt))
+
+        # test when S3 file exists
+        self.assertTrue(os.path.isfile(test_txt))
+
+        private_pem_file = os.path.join(self.resources_dir, 'unit-test-private.pem') if Signing.is_travis() else None
+        public_pem_file = os.path.join(self.resources_dir, 'unit-test-public.pem') if Signing.is_travis() else None
+        result = Signing.handle_s3_trigger(event, self.MockS3Handler, self.MockDynamodbHandler, self.MockLogger(),
+                                           private_pem_file=private_pem_file, public_pem_file=public_pem_file)
+        self.assertTrue(result)
+
+        expected_file = os.path.join(self.temp_dir, 'test.sig')
+        self.assertTrue(os.path.isfile(expected_file))
+
+    @unittest.skipIf(Signing.is_travis(), 'Skipping test_signing_handler_s3 on Travis CI.')
+    def test_signing_handler_s3(self):
+
+        # create test folder on S3
+        commit_id = str(uuid.uuid4())[-10:]
+        test_folder = 'temp/unit-test/{}'.format(commit_id)
+        s3_handler = S3Handler('test-cdn.door43.org')
+        s3_key = '{}/test.zip'.format(test_folder)
+        s3_handler.upload_file(os.path.join(self.resources_dir, 'test.zip'), s3_key)
+
+        # create a record in dynamodb
+        db_handler = DynamoDBHandler(Signing.dynamodb_table_name)
+        db_handler.insert_item({
+            'repo_name': 'unit-test',
+            'commit_id': commit_id,
+            'data': json.dumps(load_json_object(os.path.join(self.resources_dir, 'manifest.json')), sort_keys=True),
+            'timestamp': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"),
+            'files': [s3_key, ],
+            'language': 'en'
+        })
+
+        # mock a lambda event object
+        event = self.create_event()
+        event['Records'].append(self.create_s3_record(s3_handler.bucket_name, s3_key))
+
+        # do the signing
+        result = Signing.handle_s3_trigger(event, S3Handler, DynamoDBHandler, self.MockLogger())
+        self.assertTrue(result)
+
+        # check that the sig file was found
+        s3_sig = '{}/test.sig'.format(test_folder)
+        expected_file = os.path.join(self.temp_dir, 'test.sig')
+        s3_handler.download_file(s3_sig, expected_file)
+        self.assertTrue(os.path.isfile(expected_file))
+
+        # check the dynamodb record
+        row_keys = {'repo_name': 'unit-test', 'commit_id': commit_id}
+        row = db_handler.get_item(row_keys)
+
+        found_file = [f for f in row['files'] if f.endswith('test.sig')]
+        self.assertGreater(len(found_file), 0, 'The .sig file was not found in the files list.')
+
+        # add the url of the sig file to the format item
+        data = json.loads(row['data'])
+        found_file = [fmt['sig'] for fmt in data['formats'] if fmt['sig'].endswith('test.sig')]
+        self.assertGreater(len(found_file), 0, 'The .sig file was not found in the formats list.')
+
+        # clean up
+        db_handler.delete_item(row_keys)
+        s3_handler.delete_file(s3_key)
+        s3_handler.delete_file(s3_sig)
+
+    def test_signing_handler_zip(self):
+
+        # copy zip file to temp directory
+        test_zip = os.path.join(self.temp_dir, 'test.zip')
+        shutil.copy(os.path.join(self.resources_dir, 'test.zip'), test_zip)
+
+        # mock a lambda event object
+        event = self.create_event()
+        event['Records'].append(self.create_s3_record('test-cdn_bucket', test_zip))
+
+        # test when S3 file exists
+        self.assertTrue(os.path.isfile(test_zip))
+
+        private_pem_file = os.path.join(self.resources_dir, 'unit-test-private.pem') if Signing.is_travis() else None
+        public_pem_file = os.path.join(self.resources_dir, 'unit-test-public.pem') if Signing.is_travis() else None
+        result = Signing.handle_s3_trigger(event, self.MockS3Handler, self.MockDynamodbHandler, self.MockLogger(),
+                                           private_pem_file=private_pem_file, public_pem_file=public_pem_file)
+        self.assertTrue(result)
+
+        expected_file = os.path.join(self.temp_dir, 'test.sig')
+        self.assertTrue(os.path.isfile(expected_file))
+
+        # test when S3 file does not exist
+
+        # test successful sig file generation
+
+        # test unsuccessful sig file generation
+
+        # test updating DynamoDB record
+
+    def test_signing_handler_no_records(self):
+
+        # mock a lambda event object
+        event = self.create_event()
+        event.pop('Records', None)
+
+        private_pem_file = os.path.join(self.resources_dir, 'unit-test-private.pem') if Signing.is_travis() else None
+        public_pem_file = os.path.join(self.resources_dir, 'unit-test-public.pem') if Signing.is_travis() else None
+        result = Signing.handle_s3_trigger(event, self.MockS3Handler, self.MockDynamodbHandler, self.MockLogger(),
+                                           private_pem_file=private_pem_file, public_pem_file=public_pem_file)
+        self.assertFalse(result)
+
+    def test_signing_handler_no_s3(self):
+
+        # mock a lambda event object
+        event = self.create_event()
+        event['Records'].append({'one': 'two'})
+
+        private_pem_file = os.path.join(self.resources_dir, 'unit-test-private.pem') if Signing.is_travis() else None
+        public_pem_file = os.path.join(self.resources_dir, 'unit-test-public.pem') if Signing.is_travis() else None
+        result = Signing.handle_s3_trigger(event, self.MockS3Handler, self.MockDynamodbHandler, self.MockLogger(),
+                                           private_pem_file=private_pem_file, public_pem_file=public_pem_file)
+        self.assertFalse(result)
