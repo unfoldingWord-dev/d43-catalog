1837c6ecfafc1fc2029525e982e97bbe26ecd18d
Phil Hopper <phillip_hopper@wycliffeassociates.org>
2017-01-10 09:04:50 -0500

    Debugging

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 from setuptools import setup
 
 setup(
-    name="d43-catalog",
-    version="0.0.1",
-    author="unfoldingWord",
-    author_email="unfoldingword.org",
-    description="Publishing door43-catalog organization.",
-    license="MIT",
-    keywords="",
-    url="https://github.org/unfoldingWord-dev/d43-catalog",
-    long_description="Publishing door43-catalog organization.",
+    name='d43-catalog',
+    package_dir={'acceptance-test': 'functions/acceptance-test',
+                 'catalog': 'functions/catalog',
+                 'signing': 'functions/signing',
+                 'webhook': 'functions/webhook'},
+    packages=['acceptance-test', 'catalog', 'signing', 'webhook'],
+    version='0.0.1',
+    author='unfoldingWord',
+    author_email='unfoldingword.org',
+    description='Publishing door43-catalog organization.',
+    license='MIT',
+    keywords='',
+    url='https://github.org/unfoldingWord-dev/d43-catalog',
+    long_description='Publishing door43-catalog organization.',
     classifiers=[],
     install_requires=[
-        "tx-shared-tools",
-        "requests"
+        'tx-shared-tools',
+        'requests'
     ]
 )
