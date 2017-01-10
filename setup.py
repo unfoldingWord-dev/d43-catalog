94d522464fafe615839185e19bd8843ff6aecefb
Phil Hopper <phillip_hopper@wycliffeassociates.org>
2017-01-10 08:42:38 -0500

    Add setup.py file
    So the project can be installed into the testing pipeline

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name="d43-catalog",
+    version="0.0.1",
+    author="unfoldingWord",
+    author_email="unfoldingword.org",
+    description="Publishing door43-catalog organization.",
+    license="MIT",
+    keywords="",
+    url="https://github.org/unfoldingWord-dev/d43-catalog",
+    long_description="Publishing door43-catalog organization.",
+    classifiers=[],
+    install_requires=[
+        "tx-shared-tools",
+        "requests"
+    ]
+)
