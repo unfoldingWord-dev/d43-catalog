b826fe8765035464353e26cb849fef0835313cf3
Joel Lonbeck <joel@neutrinographics.com>
2017-05-16 14:15:08 -0700

    added converter file to gitignore

---
+++ functions/catalog/v2_catalog_converter.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+#
+# Class for converting the catalog into a format compatible with the v2 api.
+#
+
+
+class V2CatalogConverter:
+
+    def __init__(self, catalog):
+        """
+        Initializes the converter with the catalog from which to generate the v2 catalog
+        :param catalog: the latest catalog
+        """
+        self.latest_catalog = catalog
+
+    def convert_catalog(self):
+        """
+        Generates the v2 catalog
+        :return: the v2 form of the catalog
+        """
+        # TODO: generate the v2 catalog
+        v2_catalog = self.latest_catalog
+
+        return v2_catalog
