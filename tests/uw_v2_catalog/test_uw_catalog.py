a1abc6b7dcbb29d3119a60a32d5fe4e7e501da7c
Joel Lonbeck <joel@neutrinographics.com>
2017-05-30 17:09:00 -0700

    fixed tests for uw api

--- tests/uw_v2_catalog/test_uw_catalog.py
+++ tests/uw_v2_catalog/test_uw_catalog.py
@@ -10,29 +10,39 @@ class TestUwV2Catalog(TestCase):
     resources_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'resources')
 
     def setUp(self):
-        self.latest_catalog = load_json_object(os.path.join(TestUwV2Catalog.resources_dir, "catalog.json"))
+        self.latest_catalog = load_json_object(os.path.join(TestUwV2Catalog.resources_dir, "v3_catalog.json"))
         self.v2_catalog = load_json_object(os.path.join(TestUwV2Catalog.resources_dir, "v2_catalog.json"))
+
+    def assertObjectEqual(self, obj1, obj2):
+        """
+        Checks if two objects are equal after recursively sorting them
+        :param obj1:
+        :param obj2:
+        :return:
+        """
+        self.assertEqual(TestUwV2Catalog.ordered(obj1), TestUwV2Catalog.ordered(obj2))
+
+    @staticmethod
+    def ordered(obj):
+        """
+        Orders the values in an object
+        :param obj:
+        :return:
+        """
+        if isinstance(obj, dict):
+            return sorted((k, TestUwV2Catalog.ordered(v)) for k, v in obj.items())
+        if isinstance(obj, list):
+            return sorted(TestUwV2Catalog.ordered(x) for x in obj)
+        else:
+            return obj
 
     def test_create_v2_catalog(self):
         converter = UwV2CatalogHandler(self.latest_catalog)
         catalog = converter.convert_catalog()
-        print(json.dumps(catalog, sort_keys=True, indent=4))
-        pass
-        ###########
-        # The below lines are commented out in adherence to pushed code requiring passing tests.
-        # You may remove this comment block and un-comment the lines below when resuming
-        # development of the v2 catalog converter.
-        ###########
 
-        # converter = V2CatalogConverter(self.latest_catalog)
-        # catalog = converter.convert_catalog()
-        #
-        # self.assertIsNotNone(catalog)
-        #
-        # # sort catalog keys for comparison
-        # catalog_str = json.dumps(catalog, sort_keys=True)
-        # latest_catalog_str = json.dumps(self.latest_catalog, sort_keys=True)
-        # v2_catalog_str = json.dumps(self.v2_catalog, sort_keys=True)
-        #
-        # self.assertNotEqual(latest_catalog_str, catalog_str)
-        # self.assertEqual(v2_catalog_str, catalog_str)
+        # makes reading differences in the logs easier
+        catalog_str = json.dumps(catalog)
+
+        self.assertIsNotNone(catalog)
+        self.assertNotEqual(self.latest_catalog, catalog)
+        self.assertObjectEqual(self.v2_catalog, json.loads(catalog_str))
