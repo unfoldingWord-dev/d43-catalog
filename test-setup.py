a509919632c6cb72042b343ed55e200ba0d2d404
Joel Lonbeck <joel@neutrinographics.com>
2017-06-28 17:44:07 -0700

    timezone fixes

--- test-setup.py
+++ test-setup.py
@@ -19,7 +19,8 @@ setup(
         'usfm-tools==0.0.12',
         'd43_aws_tools==1.0.3',
         'boto3==1.4.4',
-        'python-dateutil==2.6.0'
+        'python-dateutil==2.6.0',
+        'pytz==2017.2'
     ],
     test_suite='tests'
 )
