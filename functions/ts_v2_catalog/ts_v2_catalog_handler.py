a58c3d0928bb6ac5592c2ce990c6fdc11440dbe9
Joel Lonbeck <joel@neutrinographics.com>
2017-06-29 15:54:43 -0700

    removed old chunk code

--- functions/ts_v2_catalog/ts_v2_catalog_handler.py
+++ functions/ts_v2_catalog/ts_v2_catalog_handler.py
@@ -216,9 +216,6 @@ class TsV2CatalogHandler:
                         source = self._generate_source_from_usx(source_path, resource['date_modified'])
                         # TODO: include app_words and language info
                         api_uploads.append(self._prep_data_upload('{}/{}/{}/source.json'.format(pid, lid, rid), source['source']))
-                        # TRICKY: we are using the legacy chunks which are statically hosted on the cdn
-                        # TODO: we should probably pull the chunks from the v3 api
-                        # api_uploads.append(self._prep_data_upload('{}/{}/{}/chunks.json'.format(pid, lid, rid), source['chunks']))
                         del usx_sources[source_key]
                     res_cat.append(resource)
                 api_uploads.append(self._prep_data_upload('{}/{}/resources.json'.format(pid, lid), res_cat))
@@ -927,31 +924,18 @@ class TsV2CatalogHandler:
         chapters.append(chp)
         return chapters
 
-    def _read_chunks(self, book):
-        chunks = []
-        verse_re = re.compile(r'<verse number="([0-9]*)', re.UNICODE)
-        for c in book:
-            for frame in c['frames']:
-                chunks.append({'id': frame['id'],
-                               'firstvs': verse_re.search(frame['text']).group(1),
-                               'lastvs': frame["lastvs"]
-                               })
-        return chunks
-
     def _generate_source_from_usx(self, path, date_modified):
         # use utf-8-sig to remove the byte order mark
         with codecs.open(path, 'r', encoding='utf-8-sig') as in_file:
             usx = in_file.readlines()
 
         book = self._usx_to_json(usx)
-        chunks = self._read_chunks(book)
 
         return {
             'source': {
                 'chapters': book,
                 'date_modified': date_modified
-            },
-            'chunks': chunks
+            }
         }
 
     def _retrieve_rc(self, lid, rid, url):
