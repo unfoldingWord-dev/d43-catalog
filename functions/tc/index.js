d81cda53d320c85468dc167ee34c9079376030da
RoyalSix <jay.a.scottjr@biola.edu>
2018-08-16 04:17:00 -0700

    added final touched to parser functions

--- functions/tc/index.js
+++ functions/tc/index.js
@@ -1,9 +1,13 @@
-const AWS = require('aws-sdk');
 const apiHelpers = require('./helpers/apiHelpers');
 const parseHelpers = require('./helpers/parseHelpers');
 
 exports.handle = async function(e, ctx, cb) {
   const catalog = await apiHelpers.getCatalog();
-  const res = parseHelpers.pivotOnKey(catalog, 'subject');
-  cb(null, res)
+  const {pivoted, index, subject} = parseHelpers.pivotOnKey(catalog, 'subject');
+  await apiHelpers.uploadToS3('v3/subjects/index.json', index)
+  await apiHelpers.uploadToS3('v3/subjects/pivoted.json', pivoted)
+  Object.keys(subject).forEach(async (key) => {
+    await apiHelpers.uploadToS3(`v3/subjects/${key}.json`, subject[key])
+  })
+  cb(null, 'OK')
 }
