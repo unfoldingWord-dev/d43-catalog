3c0bc20434f567248ea02d54f3160ce1f287aa3b
RoyalSix <jay.a.scottjr@biola.edu>
2018-08-20 12:32:38 -0700

    added more tests

--- functions/tc/helpers/parseHelpers.js
+++ functions/tc/helpers/parseHelpers.js
@@ -3,38 +3,40 @@ function pivotOnKey(catalog, key) {
   const newCatalog = {};
   const pluralKey = key + 's';
   const pivoted = {
-    catalogs: catalog.catalogs,
+    catalogs: catalog.catalogs || [],
     [pluralKey]: []
   };
-  catalog.languages.forEach(
-    ({identifier: language, resources, ...otherLanguage}) => {
-      resources.forEach((resource, resourceIndex) => {
-        if (resource[key]) {
-          const pivotKeyValue = resource[key].replace(/\s/ig, '_');
-          if (pivotKeyValue) {
-            const indexURL = `https://api.door43.org/v3/${pluralKey}/${pivotKeyValue}.json`;
-            if (!newCatalog[pivotKeyValue]) newCatalog[pivotKeyValue] = [];
-            newCatalog[pivotKeyValue].push(
-              Object.assign({
-                [key]: pivotKeyValue,
-                identifier: pivotKeyValue,
-                language,
-                resources: [resources[resourceIndex]]
-              }, otherLanguage));
-            if (!index.find(url => url === indexURL)) {
-              index.push(indexURL);
+  if (catalog.languages) {
+    catalog.languages.forEach(
+      ({identifier: language, resources, ...otherLanguage}) => {
+        resources.forEach((resource, resourceIndex) => {
+          if (resource[key]) {
+            const pivotKeyValue = resource[key].replace(/\s/ig, '_');
+            if (pivotKeyValue) {
+              const indexURL = `https://api.door43.org/v3/${pluralKey}/${pivotKeyValue}.json`;
+              if (!newCatalog[pivotKeyValue]) newCatalog[pivotKeyValue] = [];
+              newCatalog[pivotKeyValue].push(
+                Object.assign({
+                  [key]: pivotKeyValue,
+                  identifier: pivotKeyValue,
+                  language,
+                  resources: [resources[resourceIndex]]
+                }, otherLanguage));
+              if (!index.find(url => url === indexURL)) {
+                index.push(indexURL);
+              }
+              pivoted[pluralKey].push(
+                Object.assign({
+                  [key]: pivotKeyValue,
+                  identifier: pivotKeyValue,
+                  language,
+                  resources: [resources[resourceIndex]]
+                }, otherLanguage));
             }
-            pivoted[pluralKey].push(
-              Object.assign({
-                [key]: pivotKeyValue,
-                identifier: pivotKeyValue,
-                language,
-                resources: [resources[resourceIndex]]
-              }, otherLanguage));
           }
-        }
+        });
       });
-    });
+  }
   return {
     [key]: newCatalog,
     index,
