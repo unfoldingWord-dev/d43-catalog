b59ea789853a94604eea3616f2fb89c1f515a65d
Joel Lonbeck <joel@neutrinographics.com>
2018-09-18 13:22:22 -0700

    added chapter and verse padding when reading chunks

--- libraries/tools/ts_v2_utils.py
+++ libraries/tools/ts_v2_utils.py
@@ -101,8 +101,24 @@ def index_tn_rc(lid, temp_dir, rc_dir, reporter=None):
                 verse_body = convert_rc_links(verse_body)
                 general_notes = note_general_re.search(verse_body)
 
+                # zero pad chapter
+                padded_chapter = chapter
+                while len(padded_chapter) < 3 and padded_chapter not in chunk_json:
+                    padded_chapter = padded_chapter.zfill(len(padded_chapter) + 1)
+                    # keep padding if match is found
+                    if padded_chapter in chunk_json:
+                        chapter = padded_chapter
+
                 if pid != 'obs' and chapter not in chunk_json:
-                    raise Exception('Missing chapter "{}" key in chunk json while reading chunks for {}'.format(chapter, pid))
+                    raise Exception('Missing chapter "{}" key in chunk json while reading chunks for {}. RC: {}'.format(chapter, pid, rc_dir))
+
+                # zero pad verse
+                padded_verse = verse
+                while len(padded_verse) < 3 and chapter in chunk_json and padded_verse not in chunk_json[chapter]:
+                    padded_verse = padded_verse.zfill(len(padded_verse) + 1)
+                    # keep padding if match is found
+                    if padded_verse in chunk_json[chapter]:
+                        verse = padded_verse
 
                 # close chunk
                 if firstvs is not None and (pid == 'obs' or verse in chunk_json[chapter]):
