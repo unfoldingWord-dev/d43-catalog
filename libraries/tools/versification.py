8bc55c1ab9a671d236dabcf85dff8e60625a7a42
Joel Lonbeck <joel@neutrinographics.com>
2019-04-25 13:16:07 -0700

    added versification tests

--- libraries/tools/versification.py
+++ libraries/tools/versification.py
@@ -16,8 +16,9 @@ class Ref:
 
 
 class Opt:
-    def __init__(self, early, ref, c, v, count=1, psalm_heading=0, split=False):
+    def __init__(self, ref, c, v, delete=False, early=False, split=False, count=1, psalm_heading=0):
         self.early = early
+        self.delete = delete
         self.ref = ref
         self.c = c
         self.v = v
@@ -46,7 +47,7 @@ def hebrew_to_ufw(b, c, v, from_original=True):
             break_chapter(Opt(early=True, ref=ref, c=7, v=26, count=4))
         # verse split exo 20:13
         if c == 20 or v == 13:
-            split_verse(Opt(early=False, split=True, ref=ref, c=20, v=13, count=3))
+            split_verse(Opt(split=True, ref=ref, c=20, v=13, count=3))
         # chapter break exo 21:37
         if c == 21 or c == 22:
             break_chapter(Opt(early=True, ref=ref, c=21, v=37))
@@ -74,7 +75,7 @@ def hebrew_to_ufw(b, c, v, from_original=True):
     if b == '1sa':
         # verse split across a chapter break 1sa 20:42
         if c == 20 or c == 21:
-            split_verse_across_chapters(Opt(early=False, split=False, ref=ref, c=20, v=42))
+            split_verse_across_chapters(Opt(split=False, ref=ref, c=20, v=42))
         # chapter break 1sa 23:29
         if c == 23 or c == 24:
             break_chapter(Opt(early=False, ref=ref, c=23, v=29))
@@ -88,7 +89,7 @@ def hebrew_to_ufw(b, c, v, from_original=True):
             break_chapter(Opt(early=False, ref=ref, c=4, v=21, count=14))
         # verse split 1ki 22:43
         if c == 22:
-            split_verse(Opt(early=False, split=False, ref=ref, c=22, v=43))
+            split_verse(Opt(split=False, ref=ref, c=22, v=43))
     if b == '2ki':
         # chapter break 2ki 11:21
         if c == 11 or c == 12:
@@ -99,7 +100,7 @@ def hebrew_to_ufw(b, c, v, from_original=True):
             break_chapter(Opt(early=True, ref=ref, c=5, v=27, count=15))
         # verse split 1ch 12:4
         if c == 12:
-            split_verse(Opt(early=False, split=False, ref=ref, c=12, v=4))
+            split_verse(Opt(split=False, ref=ref, c=12, v=4))
     if b == '2ch':
         # chapter break 2ch 1:8
         if c == 1 or c == 2:
@@ -114,7 +115,7 @@ def hebrew_to_ufw(b, c, v, from_original=True):
         # verse split neh 7:67
         if c == 7:
             # NOTE: we don't currently support splitting into ufw e.g. Neh.7.67-Neh.7.68
-            # split_verse(Opt(early=False, split=True, ref=ref, c=7, v=67))
+            # split_verse(Opt(split=True, ref=ref, c=7, v=67))
             pass
         # chapter break neh 9:38
         if c == 9 or c == 10:
@@ -124,35 +125,92 @@ def hebrew_to_ufw(b, c, v, from_original=True):
         if c == 40 or c == 41:
             break_chapter(Opt(early=True, ref=ref, c=40, v=25, count=8))
     if b == 'psa':
-        pass
+        # delete psa heading 1 verse
+        chapters = [3, 4, 5, 6, 7, 8, 9, 12, 13, 18, 19, 20, 21, 22, 30, 31, 34, 36, 38, 39, 40, 41, 42, 44, 45, 46, 47,
+                    48, 49, 53, 55, 56, 57, 58, 59, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 75, 76, 77, 80, 81, 83, 84,
+                    85, 88, 89, 92, 102, 108, 140, 142]
+        if c in chapters:
+            delete_verse(Opt(delete=from_original, ref=ref, c=c, v=1))
+        # delete psa heading 2 verses
+        chapters = [51, 52, 54, 60]
+        if c in chapters:
+            delete_verse(Opt(delete=from_original, ref=ref, c=c, v=1, count=2))
+        # verse split psa 13:6
+        if c == 13:
+            # NOTE: we don't currently support splitting into ufw e.g. Ps.13.5-Ps.13.6
+            # split_verse(Opt(split=True, ref=ref, c=13, v=6, psalm_heading=1))
+            pass
+        # verse split psa 66:2
+        if c == 66:
+            # NOTE: we don't currently support splitting into ufw e.g. Ps.66.1-Ps.66.2
+            # split_verse(Opt(split=True, ref=ref, c=66, v=2, psalm_heading=1))
+            pass
     if b == 'ecc':
-        pass
+        # chapter break ecc 4:17
+        if c == 4 or c == 5:
+            break_chapter(Opt(early=True, ref=ref, c=4, v=17))
     if b == 'sng':
-        pass
+        # chapter break sng 6:13
+        if c == 6 or c == 7:
+            break_chapter(Opt(early=False, ref=ref, c=6, v=13))
     if b == 'isa':
-        pass
+        # chapter break isa 8:32
+        if c == 8 or c == 9:
+            break_chapter(Opt(early=True, ref=ref, c=8, v=23))
+        # verse split across a chapter break isa 63:19
+        if c == 63 or c == 64:
+            # NOTE: we don't currently support splitting into ufw e.g. Isa.63.19-Isa.64.1
+            # split_verse_across_chapters(Opt(split=True, ref=ref, c=63, v=19))
+            pass
     if b == 'jer':
-        pass
+        # chapter break jer 8:23
+        if c == 8 or c == 9:
+            break_chapter(Opt(early=True, ref=ref, c=8, v=23))
     if b == 'ezk':
-        pass
+        # chapter break ezk 20:45
+        if c == 20 or c == 21:
+            break_chapter(Opt(early=False, ref=ref, c=20, v=45, count=5))
     if b == 'dan':
-        pass
+        # chapter break dan 3:31
+        if c == 3 or c == 4:
+            break_chapter(Opt(early=True, ref=ref, c=3, v=31, count=3))
+        # chapter break dan 5:31
+        if c == 5 or c == 6:
+            break_chapter(Opt(early=False, ref=ref, c=5, v=31))
     if b == 'hos':
-        pass
+        # chapter break hos 1:10
+        if c == 1 or c == 2:
+            break_chapter(Opt(early=False, ref=ref, c=1, v=10, count=2))
+        # chapter break hos 11:12
+        if c == 11 or c == 12:
+            break_chapter(Opt(early=False, ref=ref, c=11, v=12))
+        # chapter break hos 13:16
+        if c == 13 or c == 14:
+            break_chapter(Opt(early=False, ref=ref, c=13, v=16))
     if b == 'jol':
         # chapter split jol 2:28
         if c >= 2:
             split_chapter(Opt(early=False, ref=ref, c=2, v=28))
     if b == 'jon':
-        pass
+        # chapter break jon 1:17
+        if c == 1 or c == 2:
+            break_chapter(Opt(early=False, ref=ref, c=1, v=17))
     if b == 'mic':
-        pass
+        # chapter break mic 4:14
+        if c == 4 or c == 5:
+            break_chapter(Opt(early=True, ref=ref, c=4, v=14))
     if b == 'nah':
-        pass
+        # chapter break nah 1:15
+        if c == 1 or c == 2:
+            break_chapter(Opt(early=False, ref=ref, c=1, v=15))
     if b == 'zec':
-        pass
+        # chapter break zec 1:18
+        if c == 1 or c == 2:
+            break_chapter(Opt(early=False, ref=ref, c=1, v=18, count=4))
     if b == 'mal':
-        pass
+        # chapter break mal 3:19
+        if c >= 3:
+            break_chapter(Opt(early=True, ref=ref, c=3, v=19))
     return ref
 
 
@@ -230,10 +288,9 @@ def split_verse_across_chapters(opt):
             opt.ref.v -= 1
 
 
-def delete_verse():
+def delete_verse(opt):
     """
     Handle verses which are completely deleted
     :return:
     """
     pass
-
--- tests/tools_tests/test_versification.py
+++ tests/tools_tests/test_versification.py
@@ -19,12 +19,273 @@ class TestVersification(TestCase):
         if os.path.isdir(self.temp_dir):
             shutil.rmtree(self.temp_dir, ignore_errors=True)
 
-    def test_jol_2_28(self):
-        ref = hebrew_to_ufw('jol', 3, 1)
-        expected = {'book': 'jol', 'chapter': 2, 'verse': 28}
+    def test_gen_32_1_is_31_55(self):
+        book = 'gen'
+        ref = hebrew_to_ufw(book, 32, 1)
+        expected = {'book': book, 'chapter': 31, 'verse': 55}
         self.assertEqual(expected, ref.to_dict())
 
-    def test_me(self):
+    def test_exo_7_26_is_8_1(self):
+        book = 'exo'
+        ref = hebrew_to_ufw(book, 7, 26)
+        expected = {'book': book, 'chapter': 8, 'verse': 1}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_exo_21_37_is_22_1(self):
+        book = 'exo'
+        ref = hebrew_to_ufw(book, 21, 37)
+        expected = {'book': book, 'chapter': 22, 'verse': 1}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_lev_5_20_is_6_1(self):
+        book = 'lev'
+        ref = hebrew_to_ufw(book, 5, 20)
+        expected = {'book': book, 'chapter': 6, 'verse': 1}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_num_17_1_is_16_36(self):
+        book = 'num'
+        ref = hebrew_to_ufw(book, 17, 1)
+        expected = {'book': book, 'chapter': 16, 'verse': 36}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_num_30_1_is_29_40(self):
+        book = 'num'
+        ref = hebrew_to_ufw(book, 30, 1)
+        expected = {'book': book, 'chapter': 29, 'verse': 40}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_deu_13_1_is_12_32(self):
+        book = 'deu'
+        ref = hebrew_to_ufw(book, 13, 1)
+        expected = {'book': book, 'chapter': 12, 'verse': 32}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_deu_23_1_is_22_30(self):
+        book = 'deu'
+        ref = hebrew_to_ufw(book, 23, 1)
+        expected = {'book': book, 'chapter': 22, 'verse': 30}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_deu_28_69_is_29_1(self):
+        book = 'deu'
+        ref = hebrew_to_ufw(book, 28, 69)
+        expected = {'book': book, 'chapter': 29, 'verse': 1}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_1sa_20_42_is_20_42(self):
+        book = '1sa'
+        ref = hebrew_to_ufw(book, 20, 42)
+        expected = {'book': book, 'chapter': 20, 'verse': 42}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_1sa_21_1_is_20_42(self):
+        book = '1sa'
+        ref = hebrew_to_ufw(book, 21, 1)
+        expected = {'book': book, 'chapter': 20, 'verse': 42}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_1sa_24_1_is_23_29(self):
+        book = '1sa'
+        ref = hebrew_to_ufw(book, 24, 1)
+        expected = {'book': book, 'chapter': 23, 'verse': 29}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_2sa_19_1_is_18_33(self):
+        book = '2sa'
+        ref = hebrew_to_ufw(book, 19, 1)
+        expected = {'book': book, 'chapter': 18, 'verse': 33}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_1ki_5_1_is_4_21(self):
+        book = '1ki'
+        ref = hebrew_to_ufw(book, 5, 1)
+        expected = {'book': book, 'chapter': 4, 'verse': 21}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_1ki_22_43_is_22_43(self):
+        book = '1ki'
+        ref = hebrew_to_ufw(book, 22, 43)
+        expected = {'book': book, 'chapter': 22, 'verse': 43}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_1ki_22_44_is_22_43(self):
+        book = '1ki'
+        ref = hebrew_to_ufw(book, 22, 44)
+        expected = {'book': book, 'chapter': 22, 'verse': 43}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_2ki_12_1_is_11_21(self):
+        book = '2ki'
+        ref = hebrew_to_ufw(book, 12, 1)
+        expected = {'book': book, 'chapter': 11, 'verse': 21}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_1ch_5_27_is_6_1(self):
+        book = '1ch'
+        ref = hebrew_to_ufw(book, 5, 27)
+        expected = {'book': book, 'chapter': 6, 'verse': 1}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_1ch_12_4_is_12_4(self):
+        book = '1ch'
+        ref = hebrew_to_ufw(book, 12, 4)
+        expected = {'book': book, 'chapter': 12, 'verse': 4}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_1ch_12_5_is_12_4(self):
+        book = '1ch'
+        ref = hebrew_to_ufw(book, 12, 5)
+        expected = {'book': book, 'chapter': 12, 'verse': 4}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_2ch_1_18_is_2_1(self):
+        book = '2ch'
+        ref = hebrew_to_ufw(book, 1, 18)
+        expected = {'book': book, 'chapter': 2, 'verse': 1}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_2ch_13_23_is_14_1(self):
+        book = '2ch'
+        ref = hebrew_to_ufw(book, 13, 23)
+        expected = {'book': book, 'chapter': 14, 'verse': 1}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_neh_3_33_is_4_1(self):
+        book = 'neh'
+        ref = hebrew_to_ufw(book, 3, 33)
+        expected = {'book': book, 'chapter': 4, 'verse': 1}
+        self.assertEqual(expected, ref.to_dict())
+
+    # def test_neh_7_67(self):
+        # NOTE: we don't currently support splitting into ufw
+        # pass
+
+    def test_neh_10_1_is_9_38(self):
+        book = 'neh'
+        ref = hebrew_to_ufw(book, 10, 1)
+        expected = {'book': book, 'chapter': 9, 'verse': 38}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_job_40_25_is_41_1(self):
+        book = 'job'
+        ref = hebrew_to_ufw(book, 40, 25)
+        expected = {'book': book, 'chapter': 41, 'verse': 1}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_psa_deletions(self):
+        # TODO: test deletions
+        pass
+
+    # def test_psa_13_6(self):
+        # NOTE: we don't currently support splitting into ufw
+        # pass
+
+    # def test_psa_66_2(self):
+        # NOTE: we don't currently support splitting into ufw
+        # pass
+
+    def test_ecc_4_17_is_5_1(self):
+        book = 'ecc'
+        ref = hebrew_to_ufw(book, 4, 17)
+        expected = {'book': book, 'chapter': 5, 'verse': 1}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_sng_7_1_is_6_13(self):
+        book = 'sng'
+        ref = hebrew_to_ufw(book, 7, 1)
+        expected = {'book': book, 'chapter': 6, 'verse': 13}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_isa_8_23_is_9_1(self):
+        book = 'isa'
+        ref = hebrew_to_ufw(book, 8, 23)
+        expected = {'book': book, 'chapter': 9, 'verse': 1}
+        self.assertEqual(expected, ref.to_dict())
+
+    # def test_isa_63_19(self):
+        # NOTE: we don't currently support splitting into ufw
+        # pass
+
+    def test_jer_8_23_is_9_1(self):
+        book = 'jer'
+        ref = hebrew_to_ufw(book, 8, 23)
+        expected = {'book': book, 'chapter': 9, 'verse': 1}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_ezk_21_1_is_20_45(self):
+        book = 'ezk'
+        ref = hebrew_to_ufw(book, 21, 1)
+        expected = {'book': book, 'chapter': 20, 'verse': 45}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_dan_3_31_is_4_1(self):
+        book = 'dan'
+        ref = hebrew_to_ufw(book, 3, 31)
+        expected = {'book': book, 'chapter': 4, 'verse': 1}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_dan_6_1_is_5_31(self):
+        book = 'dan'
+        ref = hebrew_to_ufw(book, 6, 1)
+        expected = {'book': book, 'chapter': 5, 'verse': 31}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_hos_2_1_is_1_10(self):
+        book = 'hos'
+        ref = hebrew_to_ufw(book, 2, 1)
+        expected = {'book': book, 'chapter': 1, 'verse': 10}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_hos_12_1_is_11_12(self):
+        book = 'hos'
+        ref = hebrew_to_ufw(book, 12, 1)
+        expected = {'book': book, 'chapter': 11, 'verse': 12}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_hos_14_1_is_13_16(self):
+        book = 'hos'
+        ref = hebrew_to_ufw(book, 14, 1)
+        expected = {'book': book, 'chapter': 13, 'verse': 16}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_jol_3_1_is_2_28(self):
+        book = 'jol'
+        ref = hebrew_to_ufw(book, 3, 1)
+        expected = {'book': book, 'chapter': 2, 'verse': 28}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_jon_2_1_is_1_17(self):
+        book = 'jon'
+        ref = hebrew_to_ufw(book, 2, 1)
+        expected = {'book': book, 'chapter': 1, 'verse': 17}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_mic_4_14_is_5_1(self):
+        book = 'mic'
+        ref = hebrew_to_ufw(book, 4, 14)
+        expected = {'book': book, 'chapter': 5, 'verse': 1}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_nah_2_1_is_1_15(self):
+        book = 'nah'
+        ref = hebrew_to_ufw(book, 2, 1)
+        expected = {'book': book, 'chapter': 1, 'verse': 15}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_zec_2_1_is_1_18(self):
+        book = 'zec'
+        ref = hebrew_to_ufw(book, 2, 1)
+        expected = {'book': book, 'chapter': 1, 'verse': 18}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_mal_3_19_is_4_1(self):
+        book = 'mal'
+        ref = hebrew_to_ufw(book, 3, 19)
+        expected = {'book': book, 'chapter': 4, 'verse': 1}
+        self.assertEqual(expected, ref.to_dict())
+
+    def test_processing_hbo(self):
         """
         Test downloading and processing some hebrew
         :return:
