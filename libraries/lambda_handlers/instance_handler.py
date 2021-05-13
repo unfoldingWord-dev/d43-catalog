25cdf8d476b38e4a458c8bb45d2f693c9759f247
Joel <da1nerd@pm.me>
2021-05-13 23:36:57 +0700

    fixed time comparison

--- libraries/lambda_handlers/instance_handler.py
+++ libraries/lambda_handlers/instance_handler.py
@@ -2,6 +2,8 @@
 
 from __future__ import unicode_literals, print_function
 from abc import ABCMeta
+from datetime import timedelta
+
 from libraries.lambda_handlers.handler import Handler
 from libraries.tools.lambda_utils import set_lambda_running, lambda_sec_remaining
 
@@ -17,7 +19,7 @@ class InstanceHandler(Handler):
         # check if already running
         running_db_name = '{}d43-catalog-running'.format(self.stage_prefix())
         sec_remaining = lambda_sec_remaining(self.context, running_db_name)
-        if sec_remaining > 0:
+        if sec_remaining > timedelta(seconds=0):
             self.logger.warning('Lambda started before last execution timed out ({}min remaining). Aborting.'.format(round(sec_remaining / 60)))
             return False
         else:
